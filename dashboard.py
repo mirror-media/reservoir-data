f168790eea293ad36a44fb7ec918c2ad97aac41b
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-05-20 11:17:06 +0800

    feat(dashboard): add south data

--- dashboard.py
+++ dashboard.py
@@ -10,6 +10,12 @@ __center_area1__ = ['10501','20101','10601']
 __center_area2__ = ['20202','20101']
 __center_area3__ = ['20509','20503','20202']
 
+__south_main__ = ['30502','30503','30501']
+__south_area1__ = ['30301','30302','30502','30501']
+__south_area2__ = ['30502','30501','30503']
+__south_area3__ = ['30503','30803','30901']
+__south_area4__ = ['31201']
+
 def convert_data_for_taiwan_dashboart(data:dict)->dict:
     status = {}
     north = {
@@ -32,5 +38,16 @@ def convert_data_for_taiwan_dashboart(data:dict)->dict:
         },
     }
     status['center'] = center
+
+    south = {
+        'main': [data[id] for id in __south_main__],
+        'area': {
+            'area1': [data[id] for id in __south_area1__],
+            'area2': [data[id] for id in __south_area2__],
+            'area3': [data[id] for id in __south_area3__],
+            'area4': [data[id] for id in __south_area4__],
+        },
+    }
+    status['south'] = south
 
     return {'status': status}
